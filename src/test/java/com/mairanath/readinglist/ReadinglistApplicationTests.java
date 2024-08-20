8b22f89363b272f5950344499ffee7e92a0954a7
mairanathalya <maira.nathalya@gmail.com>
2024-08-20 10:26:42 -0300

    build: initial commit

---
+++ src/test/java/com/mairanath/readinglist/ReadinglistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mairanath.readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadinglistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
